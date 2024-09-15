56012310b2ad13f89527f0c7a361eb48cb1ff355
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-15 18:54:03 +0900

    initial commit

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+require "vendor/autoload.php";
+
+use FoodServiceSimulation\FoodItems\CheeseBurger;
+use FoodServiceSimulation\FoodItems\Fettuccine;
+use FoodServiceSimulation\FoodItems\HawaiianPizza;
+use FoodServiceSimulation\FoodItems\Spaghetti;
+use FoodServiceSimulation\Persons\Employees\Cashier;
+use FoodServiceSimulation\Persons\Employees\Chef;
+use FoodServiceSimulation\Persons\Customer;
+use FoodServiceSimulation\Restaurants\Restaurant;
