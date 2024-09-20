1b68504f8b46ace5270c64ba8da0652e37ebc986
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-20 17:41:58 +0900

    chore: update entry point main.php

--- main.php
+++ main.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 require "vendor/autoload.php";
 
 use FoodServiceSimulation\FoodItems\CheeseBurger;
@@ -7,5 +9,39 @@ use FoodServiceSimulation\FoodItems\HawaiianPizza;
 use FoodServiceSimulation\FoodItems\Spaghetti;
 use FoodServiceSimulation\Persons\Employees\Cashier;
 use FoodServiceSimulation\Persons\Employees\Chef;
-use FoodServiceSimulation\Persons\Customer;
+use FoodServiceSimulation\Persons\Customers\Customer;
 use FoodServiceSimulation\Restaurants\Restaurant;
+
+
+$cheeseBurger = new CheeseBurger();
+$fettuccine = new Fettuccine();
+$hawaiianPizza = new HawaiianPizza();
+$spaghetti = new Spaghetti();
+
+
+$Inayah = new Chef("Inayah Lozano", 40, "Osaka", 1, 30);
+$Nadia = new Cashier("Nadia Valentine", 21, "Tokyo", 1, 20);
+
+$saizeriya = new Restaurant(
+	[
+		$cheeseBurger,
+		$fettuccine,
+		$hawaiianPizza,
+		$spaghetti
+	],
+	[
+		$Inayah,
+		$Nadia
+	]
+);
+
+$interestedTastesMap = [
+	"Margherita" => 1,
+	"CheeseBurger" => 2,
+	"Spaghetti" => 1
+];
+
+$Tom = new Customer("Tom", 20, "Saitama", $interestedTastesMap);
+
+$invoice = $Tom->order($saizeriya);
+$invoice->printInvoice();
